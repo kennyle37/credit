9d4aef2ebd3c05dda3ac4e474843cf6f2dc11f61
kennyle37 <kennyle37@gmail.com>
2019-10-23 12:19:37 -0700

    basic styling

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form className="form">
+        <div className="title">Card Number</div>
+        <input></input>
+        <div className="title">Card Name</div>
+        <input></input>
+        <div className="title">
+          Expiration Date
+          <div className="title">
+            MO
+          </div>
+          <div className="title">
+            Year
+          </div>
+          <div className="title">
+            CVV
+          </div>
+        </div>
+        <button>Submit</button>
+      </form>
     </div>
   );
 }
